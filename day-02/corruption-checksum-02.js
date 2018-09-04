9cae5500318a91ab5004582e379408207114fd6f
Eric Bakula-Davis <ericbakuladavis@gmail.com>
2018-09-04 00:07:30 -0700

    refactored corruption-checksum-02 and solved high-entropy-passphrases-02

--- day-02/corruption-checksum-02.js
+++ day-02/corruption-checksum-02.js
@@ -1,10 +1,9 @@
 var fs = require('fs');
-var input = fs.readFileSync("/home/eric/Code/advent-2017/day-02/input.txt", 'utf8').split('\n');
+var input = fs.readFileSync("/home/eric/Code/advent-2017/day-02/input.txt", 'utf8').split('\n').map((line) => line.split('\t'));
 
 let sum = 0;
 
 input.forEach((line) => {
-    line = line.split('\t');
     line.forEach((number, index) => {
         line.forEach((otherNumber, otherIndex) =>{
             if (otherNumber % number === 0 && otherIndex !== index )
---
+++ day-04/high-entropy-passphrases-02.js
@@ -0,0 +1,19 @@
+var fs = require('fs');
+var input = fs.readFileSync("/home/eric/Code/advent-2017/day-04/input.txt", 'utf8').split('\n').map((phrase) => phrase.split(' '));
+
+function areAnagrams(word, otherWord){
+    return word.split('').sort().toString() === otherWord.split('').sort().toString();
+}
+
+function isValid(phrase){
+    return phrase.every((word, index) => phrase.every((otherWord, otherIndex) => !areAnagrams(word, otherWord) || index === otherIndex));
+}
+
+let count = 0;
+
+input.forEach((phrase) => {
+    if (isValid(phrase))
+        count++;
+});
+
+console.log(count);
