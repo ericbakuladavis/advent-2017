6d6540275baeefd9a459d66af37796b6b37dfe16
Eric Bakula-Davis <ericbakuladavis@gmail.com>
2018-09-06 00:21:43 -0700

    refactor

--- day-02/corruption-checksum-01.js
+++ day-02/corruption-checksum-01.js
@@ -1,9 +1,6 @@
 const fs = require('fs');
-const input = fs.readFileSync(`${__dirname}/input.txt`, 'utf8').split('\n');
+const input = fs.readFileSync(`${__dirname}/input.txt`, 'utf8').split('\n').map((line) => line.split('\t'));
 
-const answer = input.reduce((sum, line) => {
-    line = line.split('\t');
-    return sum + Math.max(...line) - Math.min(...line);
-},0);
+const answer = input.reduce((sum, line) => sum + Math.max(...line) - Math.min(...line),0);
 
 console.log(answer);
