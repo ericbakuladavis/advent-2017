fe0f040af8a5838c16029328135206631afe8c6b
Eric Bakula-Davis <ericbakuladavis@gmail.com>
2018-09-10 13:46:36 -0700

    increased efficiency by removing nested loop

--- day-02-corruption-checksum/corruption-checksum-01.js
+++ day-02-corruption-checksum/corruption-checksum-01.js
@@ -1,6 +1,28 @@
 const fs = require('fs');
-const input = fs.readFileSync(`${__dirname}/input.txt`, 'utf8').split('\n').map((line) => line.split('\t'));
+const input = fs.readFileSync(`${__dirname}/input.txt`, 'utf8').split('\n').map((line) => line.split('\t').map((num) => parseInt(num)));
+//input = [[5,1,9,5],[7,5,3],[2,4,6,8]];
 
-const answer = input.reduce((sum, line) => sum + Math.max(...line) - Math.min(...line),0);
+// // easy answer
+// function getCheckSum(input){
+//     return input.reduce((sum, line) => sum + Math.max(...line) - Math.min(...line),0);
+// }
 
-console.log(answer);
+// more efficient answer?
+function getCheckSum(input){
+    let sum = 0; 
+    for (let i = 0; i < input.length; i++){
+        let line = input[i];
+        let max = line[0];
+        let min = line[0];
+        for (let j = 1; j < line.length; j++){
+            let num = line[j];
+            if (num > max)
+                max = num;
+            else if (num < min)
+                min = num;
+        }
+        sum += max - min;
+    }
+    return sum;
+}
+console.log(getCheckSum(input));
