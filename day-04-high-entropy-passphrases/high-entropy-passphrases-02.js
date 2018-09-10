447e10e5bfff68e5ea7ee86473d9b5a45a79d0dc
Eric Bakula-Davis <ericbakuladavis@gmail.com>
2018-09-10 13:50:50 -0700

    improved efficiency using a set and wrapped everything in functions

--- day-04-high-entropy-passphrases/high-entropy-passphrases-02.js
+++ day-04-high-entropy-passphrases/high-entropy-passphrases-02.js
@@ -1,29 +1,26 @@
 const fs = require('fs');
 const input = fs.readFileSync(`${__dirname}/input.txt`, 'utf8').split('\n').map((phrase) => phrase.split(' '));
 
-function areAnagrams(word, otherWord){
-    return word.split('').sort().toString() === otherWord.split('').sort().toString();
-}
-
 function containsAnagram(phrase){
-    for (let i = 0; i < phrase.length - 1; i++){
+    let seen = new Set();
+    for (let i = 0; i < phrase.length; i++){
         let word = phrase[i];
-        for (let j = i + 1; j < phrase.length; j++){
-            let otherWord = phrase[j];
-            if (areAnagrams(word, otherWord))
-                return true;
-        }
+        let sortedWord = word.split('').sort().toString();
+        if (seen.has(sortedWord))
+            return true;
+        else
+            seen.add(sortedWord);
     }
     return false;
-    // We could also replace everything in this function with this line:
-    //return phrase.every((word, index) => phrase.every((otherWord, otherIndex) => !areAnagrams(word, otherWord) || index === otherIndex));
 }
 
-let count = 0;
+function countValidPhrases(input){
+    let count = 0;
+    input.forEach((phrase) => {
+        if (!containsAnagram(phrase))
+            count++;
+    });
+    return count;
+}
 
-input.forEach((phrase) => {
-    if (!containsAnagram(phrase))
-        count++;
-});
-
-console.log(count);
+console.log(countValidPhrases(input));
