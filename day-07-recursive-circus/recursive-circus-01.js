ff4fdc2c2602222b020e3d319ea6a8f4db6b5385
Eric Bakula-Davis <ericbakuladavis@gmail.com>
2018-09-13 15:27:42 -0700

    removed complexity from addChildrenToTree and improved variable names

--- day-07-recursive-circus/recursive-circus-01.js
+++ day-07-recursive-circus/recursive-circus-01.js
@@ -1,5 +1,4 @@
-function addChildrenToTree(tree, nodeName, indices, line){
-    let node = tree[nodeName];
+function addChildrenToTree(tree, node, indices, line){
     node.children = {};
     let childrenNames = line.slice(indices.dash).split(', ');
     childrenNames.forEach((childName) => {
@@ -10,32 +9,31 @@ function addChildrenToTree(tree, nodeName, indices, line){
     });
 }
 
-function getIndices(line){
+function getCharacterIndices(line){
     let indices = {};
     for (let i = 0; !indices.dash && i < line.length ; i++){
-        let char = line[i];
-        switch (char) {
-            case '(':   indices.openParens = i;
+        let character = line[i];
+        switch (character) {
+            case '(':   indices.openParenthesis = i;
                         break;
-            case ')':   indices.closeParens = i;
+            case ')':   indices.closedParenthesis = i;
                         break;
             case '-':   indices.dash = i + 3;
                         break;
         }
-
     }
     return indices;
 }
 
 function addNodeAndChildrenToTree(tree, line){
-    let indices = getIndices(line);
-    let nodeName = line.slice(0, indices.openParens - 1);
+    let indices = getCharacterIndices(line);
+    let nodeName = line.slice(0, indices.openParenthesis - 1);
     if (!tree[nodeName])
         tree[nodeName] = {};
     let node = tree[nodeName];
-    node.weight = parseInt(line.slice(indices.openParens + 1, indices.closeParens));
+    node.weight = parseInt(line.slice(indices.openParenthesis + 1, indices.closedParenthesis));
     if (indices.dash){
-        addChildrenToTree(tree, nodeName, indices, line); 
+        addChildrenToTree(tree, node, indices, line); 
     }
 }
 
