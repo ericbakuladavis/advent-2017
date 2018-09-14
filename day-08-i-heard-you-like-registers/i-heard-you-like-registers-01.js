31d81693d704283e60e20e2e9d8002e89529fb03
Eric Bakula-Davis <ericbakuladavis@gmail.com>
2018-09-13 23:30:35 -0700

    new solution

---
+++ day-08-i-heard-you-like-registers/i-heard-you-like-registers-01.js
@@ -0,0 +1,59 @@
+function getSpaceIndices(line){
+    let spaceIndices = [];
+    for (let i = 0; spaceIndices.length < 5; i++){
+        let character = line[i];
+        if (character === ' ')
+            spaceIndices.push(i);
+    }
+    return spaceIndices;
+}
+
+function parseLine(line){
+    let spaceIndices = getSpaceIndices(line);
+    let registerName = line.slice(0, spaceIndices[0]);
+    let operation = line.slice(spaceIndices[0] + 1, spaceIndices[1]);
+    let amount = parseInt(line.slice(spaceIndices[1] + 1, spaceIndices[2]));
+    let expression = line.slice(spaceIndices[3] + 1);
+    let expressionRegisterName = line.slice(spaceIndices[3] + 1, spaceIndices[4]);
+    return {registerName, operation, amount, expression, expressionRegisterName};
+}
+
+function followInstructions(input){
+
+    let registers = {};
+    
+    input.forEach((line) => {
+
+        let instructions = parseLine(line);
+        let {registerName, operation, amount, expression, expressionRegisterName} = instructions;
+
+        // Initialize registers if they don't already exist
+        if (!registers[registerName])
+            registers[registerName] = 0;
+        if (!registers[expressionRegisterName])
+            registers[expressionRegisterName] = 0;
+        
+        // Evaluate the expression
+        expression = 'registers.' + expression; 
+        if (eval(expression)){
+
+            // Modify the register
+            if (operation === 'inc')
+                registers[registerName] += amount;
+            else
+                registers[registerName] -= amount;
+        }
+    });
+    return registers;
+}
+
+function getMaxRegister(input){
+    let registers = followInstructions(input);
+    let values = Object.values(registers);
+    return Math.max(...values); 
+}
+
+const fs = require('fs');
+const input = fs.readFileSync(`${__dirname}/input.txt`, 'utf8').split('\n');
+
+console.log(getMaxRegister(input));
