07e505d93bd7342962ce807efefc7fcd0926f3a7
Eric Bakula-Davis <ericbakuladavis@gmail.com>
2018-09-16 02:37:30 -0700

    wrapped in function

--- day-01-inverse-captcha/inverse-captcha-01.js
+++ day-01-inverse-captcha/inverse-captcha-01.js
@@ -1,17 +1,19 @@
+function inverseCaptcha(input){
+    let sum = 0;
+    for (let i = 0; i < input.length; i++){
+        let curNum = input[i];
+        let nextNum = input[i + 1];
+    
+        if (i === input.length - 1)
+            nextNum = input[0];
+    
+        if (curNum === nextNum)
+            sum += curNum;
+    }
+    return sum;
+}
+
 const fs = require('fs');
 let input = fs.readFileSync(`${__dirname}/input.txt`, 'utf8').split('').map((str) => parseInt(str));
 
-let sum = 0;
-
-for (let i = 0; i < input.length; i++){
-    let curNum = input[i];
-    let nextNum = input[i + 1];
-
-    if (i === input.length - 1)
-        nextNum = input[0];
-
-    if (curNum === nextNum)
-        sum += curNum;
-}
-
-console.log(sum);
+console.log(inverseCaptcha(input)); // 1223
--- day-01-inverse-captcha/inverse-captcha-02.js
+++ day-01-inverse-captcha/inverse-captcha-02.js
@@ -1,17 +1,17 @@
+function inverseCaptcha(input){
+    let sum = 0;
+    for (let i = 0; i < input.length / 2; i++){
+        let curNum = input[i];
+        let nextNumIndex = i + input.length / 2;
+        let nextNum = input[nextNumIndex];
+
+        if (curNum === nextNum)
+            sum += curNum + nextNum;
+    }
+    return sum;
+}
+
 const fs = require('fs');
 let input = fs.readFileSync(`${__dirname}/input.txt`, 'utf8').split('').map((str) => parseInt(str));
 
-let sum = 0;
-
-// we only loop through the first half of the numbers
-// as we go, we check each number with it's coorisponding number in the 2nd half
-for (let i = 0; i < input.length / 2; i++){
-    let curNum = input[i];
-    let nextNumIndex = i + input.length / 2;
-    let nextNum = input[nextNumIndex];
-
-    if (curNum === nextNum)
-        sum += curNum + nextNum;
-}
-
-console.log(sum);
+console.log(inverseCaptcha(input)); // 1284
