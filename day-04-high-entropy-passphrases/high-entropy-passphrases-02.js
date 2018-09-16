ffbb969fe62ce21f34cab399367d4321864d2417
Eric Bakula-Davis <ericbakuladavis@gmail.com>
2018-09-16 02:44:29 -0700

    made counter function match function from part 01. might learn how to import.

--- day-04-high-entropy-passphrases/high-entropy-passphrases-02.js
+++ day-04-high-entropy-passphrases/high-entropy-passphrases-02.js
@@ -1,26 +1,26 @@
-const fs = require('fs');
-const input = fs.readFileSync(`${__dirname}/input.txt`, 'utf8').split('\n').map((phrase) => phrase.split(' '));
-
-function containsAnagram(phrase){
+function phraseIsValid(phrase){
     let seen = new Set();
     for (let i = 0; i < phrase.length; i++){
         let word = phrase[i];
         let sortedWord = word.split('').sort().toString();
         if (seen.has(sortedWord))
-            return true;
+            return false;
         else
             seen.add(sortedWord);
     }
-    return false;
+    return true;
 }
 
 function countValidPhrases(input){
     let count = 0;
     input.forEach((phrase) => {
-        if (!containsAnagram(phrase))
+        if (phraseIsValid(phrase))
             count++;
     });
     return count;
 }
 
-console.log(countValidPhrases(input));
+const fs = require('fs');
+const input = fs.readFileSync(`${__dirname}/input.txt`, 'utf8').split('\n').map((phrase) => phrase.split(' '));
+
+console.log(countValidPhrases(input)); // 208
