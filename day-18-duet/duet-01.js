4d07fa034abd228f44ff2ce92803757cdb949029
Eric Bakula-Davis <ericbakuladavis@gmail.com>
2018-09-24 16:03:16 -0700

    new solution

---
+++ day-18-duet/duet-01.js
@@ -0,0 +1,83 @@
+function getValue(registers, string){
+    return !isNaN(string) ? parseInt(string) : registers[string];
+}
+
+function sound(registers, x){
+    let frequency = getValue(registers, x);
+    registers.lastFrequencyPlayed = frequency;
+}
+
+function set(registers, register, y){
+    const valueOfY = getValue(registers, y);
+    registers[register] = valueOfY;
+}
+
+function add(registers, register, y){
+    const valueOfY = getValue(registers, y)
+    registers[register] += valueOfY;
+}
+
+function multiply(registers, register, y){
+    const valueOfY = getValue(registers, y);
+    registers[register] *= valueOfY;
+}
+
+function modulo(registers, register, y){
+    const valueOfY = getValue(registers, y)
+    registers[register] %= valueOfY;
+}
+
+function recover(registers, register){
+    if (registers[register] !== 0){
+        return registers.lastFrequencyPlayed;
+    }
+}
+
+function jump(registers, x, y){
+    const valueOfX = getValue(registers, x);
+    const valueOfY = getValue(registers, y);
+    if (valueOfX > 0)
+        return valueOfY - 1;   
+}
+
+function parseInstruction(instruction){
+    const operationNickname = instruction[0];
+    const x = instruction[1];
+    const y = instruction[2];
+    return {operationNickname, x, y}
+}
+
+function getFirstRecoveredValue(input){
+    registers = {};
+    for (let i = 0; i < input.length && i >= 0; i++){
+        const {operationNickname, x, y} = parseInstruction(input[i]);
+        switch(operationNickname){
+            case 'snd': sound(registers, x);
+                        break;
+            case 'set': set(registers, x, y);
+                        break;
+            case 'add': add(registers, x, y);
+                        break;
+            case 'mul': multiply(registers, x, y);
+                        break;
+            case 'mod': modulo(registers, x, y);
+                        break;
+            case 'rcv': const recoverResult = recover(registers, x);
+                        if (recoverResult)   
+                            return recoverResult; 
+                        break;
+            case 'jgz': const jumpResult = jump(registers, x, y);
+                        if (jumpResult)
+                            i += jumpResult;
+                        break;
+        }
+    }
+}
+
+const fs = require('fs');
+const input = 
+fs.readFileSync(`${__dirname}/input.txt`, 'utf8')
+.split('\n')
+.map((instruction) => instruction.split(' '));
+
+console.log ( 'The first recovered frequency was', getFirstRecoveredValue(input) );
---
+++ day-18-duet/duet-02.js
@@ -0,0 +1,122 @@
+class Program {
+    constructor (programNumber){
+        this.p = programNumber;
+        this.a = 0;
+        this.b = 0;
+        this.f = 0;
+        this.i = 0;
+        this.name = 'program' + programNumber;
+        this.instructionIndex = 0;
+        this.sendQueue = [];
+        this.sendCount = 0;
+        this.status = 'active';
+        this.terminate = false;
+    }
+
+    followInstructions(input){
+        while (this.terminate === false && this.status === 'active'){
+            const instruction = input[this.instructionIndex];
+            const operationNickname = instruction[0];
+            const x = instruction[1];
+            const y = instruction[2];
+            switch(operationNickname){
+                case 'snd': this.send(x);
+                            break;
+                case 'set': this.set(x, y);
+                            break;
+                case 'add': this.add(x, y);
+                            break;
+                case 'mul': this.multiply(x, y);
+                            break;
+                case 'mod': this.modulo(x, y);
+                            break;
+                case 'rcv': this.receive(x);
+                            break;
+                case 'jgz': this.jump(x, y);
+                            break;
+            }
+            if (this.instructionIndex >= input.length || this.instructionIndex < 0){
+                this.status = 'finished';
+                this.sendQueue = [];
+            }
+            this.instructionIndex++;
+        }
+    }
+
+    send(x){
+        const valueOfX = this.getValue(x)
+        this.sendQueue.push(valueOfX);
+        this.sendCount++;
+    }
+    
+    set(register, y){
+        const valueOfY = this.getValue(y)
+        this[register] = valueOfY;
+    }
+    
+    add(register, y){
+        const valueOfY = this.getValue(y)
+        this[register] += valueOfY;
+    }
+    
+    multiply(register, y){
+        const valueOfY = this.getValue(y);
+        this[register] *= valueOfY;
+    }
+    
+    modulo(register, y){
+        const valueOfY = this.getValue(y)
+        this[register] %= valueOfY;
+    }
+    
+    receive(register){
+        if (this.partner.sendQueue.length > 0){
+            this[register] = this.partner.sendQueue.shift();
+        } else {
+            this.instructionIndex--;
+            this.status = 'paused';
+            if (this.sendQueue.length === 0){
+                this.terminate = true;
+                this.partner.terminate = true;
+            }
+        }
+    }
+
+    jump(x, y){
+        const valueOfX = this.getValue(x);
+        const valueOfY = this.getValue(y);
+        if (valueOfX > 0)
+            this.instructionIndex += (valueOfY - 1);
+    }
+
+    getValue(string){
+        return !isNaN(string) ? parseInt(string) : this[string];
+    }
+}
+
+function getSendCountOfProgram(input, programNumber){
+    const program0 = new Program(0);
+    const program1 = new Program(1);
+    program0.partner = program1;
+    program1.partner = program0;
+
+    while (program0.terminate === false && program1.terminate === false  && !(program0.status === 'finished' && program1.status === 'finished')) {
+        if (program0.status !== 'finished'){
+            program0.status = 'active';
+            program0.followInstructions(input);
+        }
+        if (program1.status !== 'finished'){
+            program1.status = 'active';
+            program1.followInstructions(input);
+        }
+    }
+    return eval('program' + programNumber + '.sendCount');
+}
+
+const fs = require('fs');
+const input = 
+fs.readFileSync(`${__dirname}/input.txt`, 'utf8')
+.split('\n')
+.map((instruction) => instruction.split(' '));
+
+console.log( 'The program sent ' + getSendCountOfProgram(input, 1) + ' values' ); // 7239
